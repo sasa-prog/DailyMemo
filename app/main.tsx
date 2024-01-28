7b62867b8e3acc1dd41e2e45ee926d59c8f3f2b6
sasa-prog <sasagodan@gmail.com>
2024-01-28 18:44:33 +0900

    add prettier

--- app/main.tsx
+++ app/main.tsx
@@ -1,15 +1,19 @@
-'use client'
+'use client';
 
-import Calendar from "@/components/calendar/calendar"
-import { SideBarEditor } from "@/components/editor"
-import { useState } from "react"
+import Calendar from '@/components/calendar/calendar';
+import { SideBarEditor } from '@/components/editor';
+import { useState } from 'react';
 
 export default function MainView() {
-    const [viewSideEditor, setViewSideEditor] = useState(false)
-    return (
-        <>
-          <Calendar clickHandler={() => {setViewSideEditor(!viewSideEditor)}}/>
-          {viewSideEditor && <SideBarEditor />}
-        </>
-    )
+  const [viewSideEditor, setViewSideEditor] = useState(false);
+  return (
+    <>
+      <Calendar
+        clickHandler={() => {
+          setViewSideEditor(!viewSideEditor);
+        }}
+      />
+      {viewSideEditor && <SideBarEditor />}
+    </>
+  );
 }
